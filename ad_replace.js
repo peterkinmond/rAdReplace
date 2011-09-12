20cbe55d54079a9c02570d6e4a97ae8372786b42
Wayne Mak <makwayne@gmail.com>
2011-09-12 19:45:55 -0400

    add height and width to replaced ad

--- ad_replace.js
+++ ad_replace.js
@@ -1,5 +1,7 @@
 $('body').click(function(e) { 
   var computedLeft = $(e.target).offset().left;
   var computedTop = $(e.target).offset().top;
-  $('body').append('<iframe src="http://google.com" style="position:absolute;top:' + computedTop + 'px;left:' + computedLeft + 'px; z-index:1000" scrolling="no" ></iframe>');
+  var computedWidth = $(e.target).css('width');
+  var computedHeight = $(e.target).css('height');
+  $('body').append('<iframe src="http://google.com" style="position:absolute;top:' + computedTop + 'px;left:' + computedLeft + 'px;width:' + computedWidth +';height:' + computedHeight +'; z-index:1000" scrolling="no" ></iframe>');
 });
