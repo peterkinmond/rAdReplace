bf225ae66c5cb0572e807bc3d1d463f1a92a6bf1
Wayne Mak <makwayne@gmail.com>
2011-09-12 20:13:07 -0400

    remove border from inserted iframe

--- ad_replace.js
+++ ad_replace.js
@@ -3,6 +3,7 @@ $('body').click(function(e) {
   var computedTop = $(e.target).offset().top;
   var computedWidth = $(e.target).css('width');
   var computedHeight = $(e.target).css('height');
-  $('body').append('<iframe src="http://google.com" style="position:absolute;top:' + computedTop + 'px;left:' + computedLeft + 'px;width:' + computedWidth +';height:' + computedHeight +'; z-index:1000" scrolling="no" ></iframe>');
+  var adUnitURL = prompt('Enter ad unit URL');  
+  $('body').append('<iframe src="' + adUnitURL + '" style="position:absolute;top:' + computedTop + 'px;left:' + computedLeft + 'px;width:' + computedWidth +';height:' + computedHeight +'; z-index:1000; border:0" scrolling="no" ></iframe>');
   e.preventDefault();
 });
