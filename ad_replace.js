712de8d9e388d2db47ccc520f7e9e0a399e2b6fa
Wayne Mak <makwayne@gmail.com>
2011-09-12 20:25:44 -0400

    wrap everything in closure to protect global namespace

--- ad_replace.js
+++ ad_replace.js
@@ -1,9 +1,14 @@
-$('body').click(function(e) { 
-  var computedLeft = $(e.target).offset().left;
-  var computedTop = $(e.target).offset().top;
-  var computedWidth = $(e.target).css('width');
-  var computedHeight = $(e.target).css('height');
-  var adUnitURL = prompt('Enter ad unit URL');  
-  $('body').append('<iframe src="' + adUnitURL + '" style="position:absolute;top:' + computedTop + 'px;left:' + computedLeft + 'px;width:' + computedWidth +';height:' + computedHeight +'; z-index:1000; border:0" scrolling="no" ></iframe>');
-  e.preventDefault();
-});
+(function() {
+  var replaceAdCallback = function(e) { 
+    var computedLeft = $(e.target).offset().left;
+    var computedTop = $(e.target).offset().top;
+    var computedWidth = $(e.target).css('width');
+    var computedHeight = $(e.target).css('height');
+    var adUnitURL = prompt('Enter ad unit URL');  
+    $('body').append('<iframe src="' + adUnitURL + '" style="position:absolute;top:' + computedTop + 'px;left:' + computedLeft + 'px;width:' + computedWidth +';height:' + computedHeight +'; z-index:1000; border:0" scrolling="no" ></iframe>');
+    e.preventDefault();
+    $('body').unbind('click', replaceAdCallback);
+  };
+
+  $('body').click(replaceAdCallback);
+})();
