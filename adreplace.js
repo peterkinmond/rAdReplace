78152f0243565f2f257cfc896edacf3dced5d518
Engineer <engineer@action.local>
2011-09-14 13:16:05 -0700

    add overlayIframe code so that we can prevent a user from clicking out from an iframe (not yet working)

--- adreplace.js
+++ adreplace.js
@@ -1,19 +1,12 @@
 // Add this as a javascript bookmarklet in order to allow this functionality 
 (function() {
   var replaceAdCallback = function(e) { 
-    load_jquery();
     var adspot = $(e.target);
     var adUnitUrl = prompt('Enter ad unit URL');  
     createOverlayAd(adUnitUrl, adspot.offset().left, adspot.offset().top, adspot.css('width'), adspot.css('height'));
 
     $('body').unbind('click', replaceAdCallback);
     e.preventDefault();
-  };
-
-  var load_jquery = function() {
-    if (typeof(jQuery) === 'undefined') {
-      document.body.innerHTML += '<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.3.js"></script>';
-    }
   };
 
   var createOverlayAd = function(adunitUrl, offsetLeft, offsetTop, width, height) {
@@ -23,3 +16,36 @@
   $('body').click(replaceAdCallback);
   alert("Click on the ad placement you'd like to replace");
 })();
+
+
+function overlayIframes() {
+  var iframes = document.body.getElementsByTagName('iframe');
+  for(var i = 0; i < iframes.length; i++) {
+    iframe = iframes[i];
+
+    var elem = document.createElement('div');
+
+    elem.id = 'smookie_' + i;
+    elem.style.position = 'absolute';
+    elem.style.left = findPos(iframe)[0];
+    elem.style.top = findPos(iframe)[1];
+    elem.style.width = iframe.offsetWidth;
+    elem.style.height = iframe.offsetHeight;
+  
+    document.body.appendChild(elem);
+  }
+}
+
+function findPos(obj) {
+	var curleft = curtop = 0;
+	if (obj.offsetParent) {
+		curleft = obj.offsetLeft;
+		curtop = obj.offsetTop;
+		while (obj = obj.offsetParent) {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		}
+	}
+	return [curleft,curtop];
+}
+
