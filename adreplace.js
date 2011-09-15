045665c95dba51bdeb949ab8fd62e57635c1c7d2
Wayne Mak <makwayne@gmail.com>
2011-09-14 18:48:04 -0700

    replace calculation of overlay add position and dimensions with non-jquery version

--- adreplace.js
+++ adreplace.js
@@ -1,23 +1,22 @@
 // Add this as a javascript bookmarklet in order to allow this functionality 
 (function() {
   var replaceAdCallback = function(e) { 
-    var adspot = $(e.target);
+    var adspot = e.target;
     var adUnitUrl = prompt('Enter ad unit URL');  
-    createOverlayAd(adUnitUrl, adspot.offset().left, adspot.offset().top, adspot.css('width'), adspot.css('height'));
+    createOverlayAd(adUnitUrl, findPos(adspot)[0], findPos(adspot)[1], getDimensions(adspot)[0], getDimensions(adspot)[1]);
 
     $('body').unbind('click', replaceAdCallback);
     e.preventDefault();
   };
 
   var createOverlayAd = function(adunitUrl, offsetLeft, offsetTop, width, height) {
-    $('body').append('<iframe src="' + adunitUrl + '" style="position:absolute;top:' + offsetTop + 'px;left:' + offsetLeft + 'px;width:' + width +';height:' + height +'; z-index:1000; border:0" scrolling="no" ></iframe>');
+    $('body').append('<iframe src="' + adunitUrl + '" style="position:absolute;top:' + offsetTop + 'px;left:' + offsetLeft + 'px;width:' + width +'px;height:' + height +'px; z-index:1000; border:0" scrolling="no" ></iframe>');
   };
 
   var overlayIframes = function() {
     var iframes = document.body.getElementsByTagName('iframe');
     for(var i = 0; i < iframes.length; i++) {
       iframe = iframes[i];
-      console.log(iframe);
 
       var elem = document.createElement('div');
 
