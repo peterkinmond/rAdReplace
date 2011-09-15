749baae013559f93db0e07480c796324a5b5da39
Wayne Mak <makwayne@gmail.com>
2011-09-14 18:32:11 -0700

    add code to handle iframes - we overlay iframes with divs so they don't lead to click out

--- adreplace.js
+++ adreplace.js
@@ -13,45 +13,40 @@
     $('body').append('<iframe src="' + adunitUrl + '" style="position:absolute;top:' + offsetTop + 'px;left:' + offsetLeft + 'px;width:' + width +';height:' + height +'; z-index:1000; border:0" scrolling="no" ></iframe>');
   };
 
+  var overlayIframes = function() {
+    var iframes = document.body.getElementsByTagName('iframe');
+    for(var i = 0; i < iframes.length; i++) {
+      iframe = iframes[i];
+      console.log(iframe);
+
+      var elem = document.createElement('div');
+
+      elem.style.position = 'absolute';
+      elem.style.left = findPos(iframe)[0] + 'px';
+      elem.style.top = findPos(iframe)[1] + 'px';
+      elem.style.width = iframe.offsetWidth + 'px';
+      elem.style.height = iframe.offsetHeight + 'px';
+    
+      document.body.appendChild(elem);
+    }
+  };
+
+  var findPos = function(obj) {
+    var curleft = curtop = 0;
+    if (obj.offsetParent) {
+      curleft = obj.offsetLeft;
+      curtop = obj.offsetTop;
+      while (obj = obj.offsetParent) {
+        curleft += obj.offsetLeft;
+        curtop += obj.offsetTop;
+      }
+    }
+    return [curleft,curtop];
+  };
+
+  overlayIframes();
   $('body').click(replaceAdCallback);
   alert("Click on the ad placement you'd like to replace");
 })();
 
-
-function overlayIframes() {
-  var iframes = document.body.getElementsByTagName('iframe');
-  for(var i = 0; i < iframes.length; i++) {
-    iframe = iframes[i];
-    console.log(iframe);
-
-    var elem = document.createElement('div');
-
-    elem.id = 'smookie_' + i;
-    elem.style.position = 'absolute';
-    elem.style.left = findPos(iframe)[0] + 'px';
-    elem.style.top = findPos(iframe)[1] + 'px';
-    elem.style.width = iframe.offsetWidth + 'px';
-    elem.style.height = iframe.offsetHeight + 'px';
-
-    console.log(findPos(iframe)[0]);
-    console.log(findPos(iframe)[1]);
-    console.log(iframe.offsetWidth);
-    console.log(iframe.offsetHeight);
-  
-    document.body.appendChild(elem);
-  }
-}
-
-function findPos(obj) {
-	var curleft = curtop = 0;
-	if (obj.offsetParent) {
-		curleft = obj.offsetLeft;
-		curtop = obj.offsetTop;
-		while (obj = obj.offsetParent) {
-			curleft += obj.offsetLeft;
-			curtop += obj.offsetTop;
-		}
-	}
-	return [curleft,curtop];
-}
 
